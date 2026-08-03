36d5e4ba1a8c229379a31f3d395bfbc52abccd75
Agostina <agosarias99@gmail.com>
2026-08-04 01:18:23 +0200

    test: add patient constructor validation tests

--- test/CarePathGuardian.UnitTests/Patients/PatientTests.cs
+++ test/CarePathGuardian.UnitTests/Patients/PatientTests.cs
@@ -1,4 +1,5 @@
-﻿using CarePathGuardian.Domain.Patients;
+﻿using System.Security.Cryptography.X509Certificates;
+using CarePathGuardian.Domain.Patients;
 
 namespace CarePathGuardian.UniTests.Patients;
 
@@ -22,4 +23,74 @@ public class PatientTests
         Assert.Equal("dateBirth", exception.ParamName);
 
     }
+
+    [Fact]
+    public void Constructor_WhenExternalReferenceIsEmpty_ShouldThrowArgumentException()
+    {
+        Action action = () => new Patient(
+            "",
+            "Ana",
+            "Garcia",
+            DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-18)),
+            "ana@gmail.com",
+            null
+        );
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("externalReference", exception.ParamName);
+    }
+
+    [Fact]
+    public void Constructor_WhenFirstNameIsEmpty_ShouldThrowArgumentException()
+    {
+        Action action = () => new Patient(
+            "PAT-001",
+            "",
+            "Garcia",
+            DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-18)),
+            "ana@gmail.com",
+            null
+        );
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("firstName", exception.ParamName);
+    }
+
+    [Fact]
+    public void Constructor_WhenLastNameIsEmpty_ShouldThrowArgumentException()
+    {
+        Action action = () => new Patient(
+            "PAT-001",
+            "Ana",
+            "",
+            DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-18)),
+            "ana@gmail.com",
+            null
+        );
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("lastName", exception.ParamName);
+    }
+
+    [Fact]
+    public void Constructor_WhenDataIsValid_ShouldCreatePatient()
+    {        
+        var dateOfBirth = new DateOnly(1995,5,20);
+
+        var patient = new Patient(
+        "PAT-001",
+        "Ana",
+        "Garcia",
+        dateOfBirth,
+        "ana@gmail.com",
+        "+353871234567");
+    
+    Assert.NotEqual(Guid.Empty,patient.Id);
+    Assert.Equal("PAT-001", patient.ExternalReference);
+    Assert.Equal("Ana",patient.FirstName);
+    Assert.Equal("Garcia",patient.LastName);
+    Assert.Equal(dateOfBirth,patient.DateOfBirth);
+    Assert.Equal("ana@gmail.com",patient.Email);
+    Assert.Equal("+353871234567",patient.PhoneNumber);
+    Assert.True(patient.CreatedAtUtc <= DateTime.UtcNow);
+    Assert.True(patient.UpdatedAtUtc <= DateTime.UtcNow);    
+    }
+
 }
